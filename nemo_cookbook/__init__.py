18fca68eb8e31e80b164c70bb2a2777c3f9bcaf3
Ollie Tooth <oliver.tooth@noc.ac.uk>
2025-06-09 17:38:14 +0100

    Add function + recipe for conservative vertical coordinate transformation.

--- nemo_cookbook/__init__.py
+++ nemo_cookbook/__init__.py
@@ -7,3 +7,4 @@ from nemo_cookbook.src.moc import compute_moc_z, compute_moc_tracer, compute_sec
 from nemo_cookbook.src.transport import compute_mht, compute_mst
 from nemo_cookbook.src.wmt import compute_volume_census, compute_sfoc_sigma0, compute_ssd_area
 from nemo_cookbook.src.extract import extract_section
+from nemo_cookbook.src.transform import transform_vertical_coords
