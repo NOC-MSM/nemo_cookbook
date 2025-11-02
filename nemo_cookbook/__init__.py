c7c9e65bb9a34ba61675b46e82d5f6e33eb6cb41
Ollie Tooth <oliver.tooth@noc.ac.uk>
2025-11-02 21:09:52 +0000

    Add compute_binned_statistic() utility function to stats module.

--- nemo_cookbook/__init__.py
+++ nemo_cookbook/__init__.py
@@ -3,6 +3,5 @@ __version__ = "0.1.0"
 __author__ = "Ollie Tooth"
 __credits__ = "National Oceanography Centre"
 
-from nemo_cookbook import examples, extract, masks, transform
-
+from nemo_cookbook import examples, extract, masks, transform, stats
 from .nemodatatree import NEMODataTree
