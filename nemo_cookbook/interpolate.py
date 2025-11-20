34868e99e9faa6bfaa2e7ea3bbe6ca3fb2a25b86
Ollie Tooth <oliver.tooth@noc.ac.uk>
2025-11-20 17:41:53 +0000

    Add masked_average utility func & update interpolate_grid() to handle domain masking.

--- nemo_cookbook/interpolate.py
+++ nemo_cookbook/interpolate.py
@@ -8,23 +8,60 @@ horizontal grids of NEMO ocean general circulation model domains.
 Author:
 Ollie Tooth (oliver.tooth@noc.ac.uk)
 """
+import numpy as np
 import xarray as xr
+
+
+def masked_average(
+    da_list: list[xr.DataArray],
+    mask_list: list[xr.DataArray]
+) -> xr.DataArray:
+    """
+    Compute the average of a DataArray collection
+    excluding masked values.
+
+    Parameters
+    ----------
+    da_list : list[xr.DataArray]
+        DataArray collection to be averaged.
+    mask_list : list[xr.DataArray]
+        Mask collection to apply to DataArrays.
+
+    Returns
+    -------
+    xr.DataArray
+        Masked average of a DataArray collection.
+    """
+    # Define sum of non-masked DataArray values:
+    data_sum = sum([xr.where(mask, da, 0) for da, mask in zip(da_list, mask_list)])
+
+    # Determine number of non-masked values:
+    mask_sum = sum([mask.fillna(False).astype(int) for mask in mask_list])
+    mask_sum = xr.where(mask_sum, mask_sum, np.nan)
+
+    # Compute masked average:
+    result = data_sum / mask_sum
+
+    return result
 
 
 def interpolate_grid(
     da: xr.DataArray,
+    mask: xr.DataArray,
     source_grid: str,
     target_grid: str,
     iperio: bool = False,
     ijk_names: tuple = ('i', 'j', 'k')
 ) -> xr.DataArray:
     """
-    Interpolate variable between NEMO horizontal grids.
+    Interpolate variable onto neighbouring NEMO horizontal grid.
 
     Parameters:
     -----------
     da : xr.DataArray
-        DataArray defined on source grid.
+        Variable defined on source grid.
+    mask : xr.DataArray
+        Mask defined on source grid.
     source_grid : str
         Source grid type ('T', 'U', 'V', or 'F').
     target_grid : str
@@ -38,10 +75,10 @@ def interpolate_grid(
     Returns:
     --------
     xr.DataArray
-        Variable linearly interpolated onto NEMO target grid.
+        Variable interpolated onto target grid.
     """
     # -- Get dimension names -- #
-    i_name, j_name, k_name = ijk_names
+    i_name, j_name, _ = ijk_names
 
     match source_grid:
         # -- Linearly interpolate T-grid scalar variables -- #
@@ -50,29 +87,43 @@ def interpolate_grid(
                 # T -> U grid:
                 if iperio:
                     # Zonally periodic domain:
-                    da_interp = 0.5 * (da.roll({i_name: 1}) + da)
+                    result = masked_average(da_list=[da.roll({i_name: 1}), da],
+                                            mask_list=[mask.roll({i_name: 1}), mask]
+                                            )
                 else:
                     # Non-periodic domain:
-                    da_interp = 0.5 * (da + da.shift({i_name: -1}))
+                    result = masked_average(da_list=[da, da.shift({i_name: -1})],
+                                            mask_list=[mask, mask.shift({i_name: -1})]
+                                            )
             elif target_grid == "V":
                 # T -> V grid:
-                da_interp = 0.5 * (da + da.shift({j_name: -1}))
+                result = masked_average(da_list=[da, da.shift({j_name: -1})],
+                                        mask_list=[mask, mask.shift({j_name: -1})]
+                                        )
             elif target_grid == "F":
                 # T -> F grid:
                 if iperio:
                     # Zonally periodic domain:
-                    da_interp = 0.25 * (da.roll({i_name: 1}).shift({j_name: -1}) +
-                                        da.roll({i_name: 1}) +
-                                        da.shift({j_name: -1}) +
-                                        da
-                                        )
+                    result = masked_average(da_list=[da.roll({i_name: 1}).shift({j_name: -1}),
+                                                     da.roll({i_name: 1}),
+                                                     da.shift({j_name: -1}),
+                                                     da],
+                                            mask_list=[mask.roll({i_name: 1}).shift({j_name: -1}),
+                                                       mask.roll({i_name: 1}),
+                                                       mask.shift({j_name: -1}),
+                                                       mask]
+                                            )
                 else:
                     # Non-periodic domain:
-                    da_interp = 0.25 * (da.shift({i_name: -1}).shift({j_name: -1}) +
-                                        da.shift({i_name: -1}) +
-                                        da.shift({j_name: -1}) +
-                                        da
-                                        )
+                    result = masked_average(da_list=[da.shift({i_name: -1}).shift({j_name: -1}),
+                                                     da.shift({i_name: -1}),
+                                                     da.shift({j_name: -1}),
+                                                     da],
+                                            mask_list=[mask.shift({i_name: -1}).shift({j_name: -1}),
+                                                       mask.shift({i_name: -1}),
+                                                       mask.shift({j_name: -1}),
+                                                       mask]
+                                            )
             else:
                 raise ValueError(f"Unsupported grid transformation from '{source_grid}' to '{target_grid}'.")
     
@@ -82,26 +133,38 @@ def interpolate_grid(
                 # U -> T grid:
                 if iperio:
                     # Zonally periodic domain:
-                    da_interp = 0.5 * (da + da.roll({i_name: 1}))
+                    result = masked_average(da_list=[da.roll({i_name: 1}), da],
+                                            mask_list=[mask.roll({i_name: 1}), mask]
+                                            )
                 else:
                     # Non-periodic domain:
-                    da_interp = 0.5 * (da + da.shift({i_name: 1}))
+                    result = masked_average(da_list=[da, da.shift({i_name: -1})],
+                                            mask_list=[mask, mask.shift({i_name: -1})]
+                                            )
             elif target_grid == "V":
                 # U -> V grid:
                 if iperio:
                     # Zonally periodic domain:
-                    da_interp = 0.25 * (da.roll({i_name: 1}).shift({j_name: 1}) +
-                                        da.roll({i_name: 1}) +
-                                        da.shift({j_name: 1}) +
-                                        da
-                                        )
+                    result = masked_average(da_list=[da.roll({i_name: 1}).shift({j_name: 1}),
+                                                     da.roll({i_name: 1}),
+                                                     da.shift({j_name: 1}),
+                                                     da],
+                                            mask_list=[mask.roll({i_name: 1}).shift({j_name: 1}),
+                                                       mask.roll({i_name: 1}),
+                                                       mask.shift({j_name: 1}),
+                                                       mask]
+                                            )
                 else:
                     # Non-periodic domain:
-                    da_interp = 0.25 * (da.shift({i_name: 1}).shift({j_name: 1}) +
-                                        da.shift({i_name: 1}) +
-                                        da.shift({j_name: 1}) +
-                                        da
-                                        )
+                    result = masked_average(da_list=[da.shift({i_name: 1}).shift({j_name: 1}),
+                                                     da.shift({i_name: 1}),
+                                                     da.shift({j_name: 1}),
+                                                     da],
+                                            mask_list=[mask.shift({i_name: 1}).shift({j_name: 1}),
+                                                       mask.shift({i_name: 1}),
+                                                       mask.shift({j_name: 1}),
+                                                       mask]
+                                            )
             else:
                 raise ValueError(f"Unsupported grid transformation from '{source_grid}' to '{target_grid}'.")
     
@@ -109,18 +172,24 @@ def interpolate_grid(
         case "V":
             if target_grid == "T":
                 # V -> T grid:
-                da_interp = 0.5 * (da + da.shift({j_name: 1}))
+                result = masked_average(da_list=[da, da.shift({j_name: 1})],
+                                        mask_list=[mask, mask.shift({j_name: 1})]
+                                        )
             elif target_grid == "U":
                 # V -> U grid:
-                da_interp = 0.25 * (da.shift({i_name: 1}).shift({j_name: 1}) +
-                                    da.shift({i_name: 1}) +
-                                    da.shift({j_name: 1}) +
-                                    da
-                                    )
+                result = masked_average(da_list=[da.shift({i_name: 1}).shift({j_name: 1}),
+                                                 da.shift({i_name: 1}),
+                                                 da.shift({j_name: 1}),
+                                                 da],
+                                        mask_list=[mask.shift({i_name: 1}).shift({j_name: 1}),
+                                                   mask.shift({i_name: 1}),
+                                                   mask.shift({j_name: 1}),
+                                                   mask]
+                                        )
             else:
                 raise ValueError(f"Unsupported grid transformation from '{source_grid}' to '{target_grid}'.")
 
         case _:
             raise ValueError(f"Unsupported source grid '{source_grid}'.")
 
-    return da_interp
+    return result
