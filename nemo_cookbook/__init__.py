b74330207ed7d50b490d7c88cc589a8291a01441
Ollie Tooth <oliver.tooth@noc.ac.uk>
2026-01-18 18:55:23 +0000

    Prepare v2026.01.0b1 beta release & remove legacy modules.

--- nemo_cookbook/__init__.py
+++ nemo_cookbook/__init__.py
@@ -1,6 +1,6 @@
 """nemo_cookbook python package."""
-__version__ = "0.1.0"
-__author__ = "Ollie Tooth"
+__version__ = "2026.1.0b1"
+__author__ = "Ollie Tooth (oliver.tooth@noc.ac.uk)"
 __credits__ = "National Oceanography Centre"
 
 from nemo_cookbook import examples, extract, masks, transform, stats
