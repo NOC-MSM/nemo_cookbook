862e9186192f1a59e4b3114571139d54cd2ea203
Ollie Tooth <oliver.tooth@noc.ac.uk>
2026-03-01 17:25:36 +0000

    Update dynamic versioning & package descriptions.

--- nemo_cookbook/__init__.py
+++ nemo_cookbook/__init__.py
@@ -1,7 +1,22 @@
-"""nemo_cookbook python package."""
-__version__ = "2026.1.0b1"
-__author__ = "Ollie Tooth (oliver.tooth@noc.ac.uk)"
-__credits__ = "National Oceanography Centre"
+"""
+NEMO Cookbook
 
-from nemo_cookbook import examples, extract, masks, transform, stats
+Reproducible analysis of NEMO ocean general circulation model outputs using xarray.
+"""
+__author__ = "Ollie Tooth (oliver.tooth@noc.ac.uk)"
+__credits__ = "National Oceanography Centre (NOC), Southampton, UK"
+
+from importlib.metadata import version as _version
+
+from nemo_cookbook import examples, extract, masks, stats, transform
+
 from .nemodatatree import NEMODataTree
+
+try:
+    __version__ = _version("nemo_cookbook")
+except Exception:
+    # Local copy or not installed with setuptools.
+    # Disable minimum version checks on downstream libraries.
+    __version__ = "9999.0.0"
+
+__all__ = ("NEMODataTree", "examples", "extract", "masks", "transform", "stats")
